3f918db7f29f5a005c5d1fbd71e82c7877e8ab4a
samuel-faith <adelajasamuel02@gmail.com>
2023-02-19 00:42:28 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with (puts) function
  *
  * Return: Always 0 (Success)
 */
