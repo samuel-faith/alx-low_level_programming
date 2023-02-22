c76846e80bf856cd76d19831f1eae26427a7a483
samuel-faith <adelajasamuel02@gmail.com>
2023-02-21 16:40:22 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
 /**
 * description - function that checks for alphabetic character.
 *
-*_isalpha - checks for alphabetic character.
+* _isalpha - checks for alphabetic character.
 *
-*@c: checks input of function
+* @c: checks input of function
 *
 *Return: return 1 if c is a letter, lowercase or uppercase, otherwise returns 0 (success)
 */
