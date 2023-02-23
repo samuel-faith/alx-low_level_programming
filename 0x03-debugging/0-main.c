c404cc200b07ce7dcd1ac6b311a62440f29e5406
samuel-faith <adelajasamuel02@gmail.com>
2023-02-23 04:22:38 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - a program that prints a random number
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+	/* code by SammieDevs*/
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
