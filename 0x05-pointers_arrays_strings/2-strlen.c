82fa4b1f575b3ebfc0c251e9d5daa04db718a32a
samuel-faith <adelajasamuel02@gmail.com>
2023-02-27 10:39:31 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string.
+*@s: char to check
+*
+* Return: 0 (success)
+*/
+
+int _strlen(char *s)
+
+{
+	int q = 0;
+	for (; *s++;)
+		q++;
+	return (q);
+
+}
