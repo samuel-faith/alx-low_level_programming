1766f4f584ed67e883f57f768daf4323c2ca5b35
samuel-faith <adelajasamuel02@gmail.com>
2023-03-08 14:21:37 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string
+*
+*@s: string
+*/
+
+void _puts_recursion(char *s)
+
+{
+	if(*s)
+	{
+		_putchar(s);
+		_puts_recursions(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+		
