bef44cf31d8832ef75473c76ff922715020f83b7
samuel-faith <adelajasamuel02@gmail.com>
2023-03-13 11:02:57 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_strdup() - returns a pointer to a newly allocated space in memory
+* _strdup() - returns a pointer to a newly allocated space in memory
 *
 *@str: string
 *
