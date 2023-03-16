9bfbdbc36f24d2e057ab1a88c44ba2ea2a83ffd8
samuel-faith <adelajasamuel02@gmail.com>
2023-03-16 10:44:11 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,30 +1,40 @@
 #include "main.h"
 
 /**
- * array_range - creates an array of integers.
- * @min: minimum range of values
- * @max: maximum range of values
+ * _realloc - reallocates a memory block using malloc and free
  *
- * Return: the pointer to the newly created array
+ * @ptr: pointer to the memory previously
+ * @old_size: allocated space for ptr
+ * @new_size: new memory block
+ *
+ * Return: pointer or NULL
  */
 
-int *array_range(int min, int max)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *ptr;
-	int i, size;
-
-	if (min > max)
-		return (NULL);
-
-	size = max - min + 1;
-
-	ptr = malloc(sizeof(int) * size);
+	char *p;
+	unsigned int i, n = new_size;
+	char *oldp = ptr;
 
 	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
-
-	for (i = 0; min <= max; i++)
-		ptr[i] = min++;
-
-	return (ptr);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		n = old_size;
+	for (i = 0; i < n; i++)
+		p[i] = oldp[i];
+	free(ptr);
+	return (p);
 }
