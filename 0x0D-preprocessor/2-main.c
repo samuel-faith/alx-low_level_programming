5b373ae157503e2154ca7d5ba952f10d64a614aa
samuel-faith <adelajasamuel02@gmail.com>
2023-03-20 15:30:14 -0700

    Display filename on compilation

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Code to print the file it was compiled from
+*
+*Return: 0 success
+*/
+
+int main()
+
+{
+	printf("The name of the file is: %s\n", __FILE__);
+
+	return (0);
+}
