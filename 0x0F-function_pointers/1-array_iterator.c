0d811862ea6cdbbb4597b0fa092b9339176e3aca
samuel-faith <adelajasamuel02@gmail.com>
2023-03-23 10:01:13 -0700

    Add implementation of array_iterator function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes a function
+*
+* @array: the array
+*
+* @size: size of the array
+*
+* @action: pointer to the function you need to use
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
