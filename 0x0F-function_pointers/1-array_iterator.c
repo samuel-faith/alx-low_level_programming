b3b660162f1468148d66c6be0e957c64a40443a2
samuel-faith <adelajasamuel02@gmail.com>
2023-03-23 10:20:35 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 
 /**
-* array_iterator - executes a function
-*
-* @array: the array
-*
-* @size: size of the array
-*
-* @action: pointer to the function you need to use
-*/
+ * array_iterator - executes a function
+ * given as a parameter on each element of an array.
+ *
+ * @array: the array
+ * @size: size of the array
+ * @action: pointer to the function you need to use
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
-
 {
 	size_t i;
 
