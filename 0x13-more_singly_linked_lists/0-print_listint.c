e0723fb545d2dec912b47ebd286211d4578caa53
samuel-faith <adelajasamuel02@gmail.com>
2023-04-03 12:25:06 -0700

    More Singly Linked Lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a listint_t list.
+*@h: pointer to the first node
+*Return: n, no of nodes.
+*/
+
+size_t print_listint(const listint_t *h)
+
+{
+	size_t no_of_nodes = 1;
+
+	/* Empty list*/
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		/*prints the first node*/
+		printf("%d", h -> n);
+		
+		/*moves to the next node*/
+		h = h -> next;
+
+		/*counts the no of nodes*/
+		no_of_nodes++;
+	}
+
+	/*prints thr last node*/
+	printf("%d", h -> n);
+
+	return (no_of_nodes);
+}
