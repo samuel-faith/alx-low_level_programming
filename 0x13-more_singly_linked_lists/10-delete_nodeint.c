d0c62cad9a731d00391ff11a5d3b1fc09e1a7a33
samuel-faith <adelajasamuel02@gmail.com>
2023-04-05 00:00:59 -0700

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,49 +1,42 @@
 #include "lists.h"
 
 /**
-* delete_nodeint_at_index - deletes the node at
-* index of a listint_t linked list.
+* delete_nodeint_at_index - deletes the node at index of a listint_t linked list.
 * @head: pointer to a pointer to the head node of the linked list
 * @index: index of the node that should be deleted, starting from 0
+*
 * Return: 1 if it succeeded, -1 if it failed
 */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 {
-	listint_t *temp, *ptr = NULL;
+
+	listint_t *current, *previous;
 	unsigned int i;
 
-	if (head == NULL)
-		return (-1);
+	if (head == NULL || *head == NULL) /* Check if the list is empty */
+	return (-1);
 
-	temp = *head;
+	current = *head;
 
 	if (index == 0)
 	{
-		*head = temp->next;
-		free(temp);
+		*head = current->next;
+		free(current);
 		return (1);
 	}
 
-	if (index == 1)
+	for (i = 0; current != NULL && i < index; i++)
 	{
-		ptr->next = temp->next;
-		free(temp);
-		return (1);
+		previous = current;
+		current = current->next;
 	}
 
-	for (i = 0; temp != NULL && i < index; i++)
-	{
-		ptr = temp;
-		temp = temp->next;
-	}
-
-	if (temp == NULL)
+	if (current == NULL) /* If the index is out of range */
 		return (-1);
 
-	ptr->next = temp->next;
-	free(temp);
+	previous->next = current->next;
+	free(current); /* Free the memory for the deleted node */
 
 	return (1);
 }
