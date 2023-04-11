616254508fece9121f8657c0c0a12ee395eb3890
samuel-faith <adelajasamuel02@gmail.com>
2023-04-11 00:00:16 -0700

    File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: path to file
+* @text_content: content
+* Return: 1 0r -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+
+{
+	FILE *sam;
+
+	size_t length;
+
+	if (!filename)
+		return (-1);
+
+	file = fopen(filename, "a")
+
+	if (!sam)
+		return (-1);
+
+	if (text_content)
+	{
+		length = strlen(text_content);
+		if (fwrite(text_content, sizeof(char), length, sam) != length)
+		{
+			fclose(sam);
+			return (-1);
+		}
+	}
+
+	fclose(sam);
+
+	return (1);
+}
