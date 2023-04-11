172193c3956af92d03611372241c7f1e7fef3770
samuel-faith <adelajasamuel02@gmail.com>
2023-04-11 00:11:12 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = fopen(filename, "a")
+	sam = fopen(filename, "a");
 
 	if (!sam)
 		return (-1);
